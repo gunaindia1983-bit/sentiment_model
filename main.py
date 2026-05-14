8e41c554676d8f1d5f7f361a5b1d491a0afd2ee0
gunaindia1983-bit <gunaindia1983@gmail.com>
2026-05-14 13:46:23 -0400

    Change app host to Render URL

--- main.py
+++ main.py
@@ -60,4 +60,4 @@ def analyze_sentiment():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='https://sentiment-model-cl77.onrender.com', port=int(os.environ.get('PORT', 5000)))
